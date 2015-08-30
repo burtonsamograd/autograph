51b161556f3b72b613565df806ccfcbf5166be92
Burton Samograd <burton.samograd@gmail.com>
2015-08-30 12:10:41 -0600

    Replace hacked version of encode-js-identifier with original code from Parenscript.

--- autograph.lisp
+++ autograph.lisp
@@ -10,15 +10,14 @@ following transformation heuristics case conversion. For example,
 paren-script becomes parenScript, *some-global* becomes SOMEGLOBAL."
     (or (gethash identifier cache)
         (setf (gethash identifier cache)
-              (cond ((some (lambda (c) (find c "-+!?#%/=:<>^")) identifier)
+              (cond ((some (lambda (c) (find c "-*+!?#@%/=:<>^")) identifier)
                      (let ((lowercase t)
                            (all-uppercase nil))
                        (when (and (not (string= identifier "[]")) ;; HACK
                                   (find-if (lambda (x) (find x '(#\. #\[ #\]))) identifier))
                          (warn "Symbol ~A contains one of '.[]' - this compound naming convention is no longer supported by Parenscript!"
                                identifier))
-                       (acond ((nth-value 1 (cl-ppcre:scan-to-strings "[\\*|\\+](.+)[\\*|\\+](.*)"
-                                                                      identifier :sharedp t))
+                       (acond ((nth-value 1 (cl-ppcre:scan-to-strings "[\\*|\\+](.+)[\\*|\\+](.*)" identifier :sharedp t))
                                (setf all-uppercase t
                                      identifier (concatenate 'string (aref it 0) (aref it 1))))
                               ((and (> (length identifier) 1)
@@ -30,16 +29,18 @@ paren-script becomes parenScript, *some-global* becomes SOMEGLOBAL."
                          (loop for c across identifier
                             do (acond ((eql c #\-)
                                        (setf lowercase (not lowercase)))
-                                      (t (write-char (cond ((and lowercase
-                                                                 (not all-uppercase)) 
-                                                            (char-downcase c))
+                                      ((position c "!?#@%+*/=:<>^")
+                                       (write-sequence (aref #("bang" "what" "hash" "at" "percent"
+                                                               "plus" "star" "slash" "equals" "colon"
+                                                               "lessthan" "greaterthan" "caret")
+                                                             it)
+                                                       acc))
+                                      (t (write-char (cond ((and lowercase (not all-uppercase)) (char-downcase c))
                                                            (t (char-upcase c)))
                                                      acc)
                                          (setf lowercase t)))))))
-                    ((every #'upper-case-p (remove-if-not #'alpha-char-p identifier)) 
-                     (string-downcase identifier))
-                    ((every #'lower-case-p (remove-if-not #'alpha-char-p identifier)) 
-                     (string-upcase identifier))
+                    ((every #'upper-case-p (remove-if-not #'alpha-char-p identifier)) (string-downcase identifier))
+                    ((every #'lower-case-p (remove-if-not #'alpha-char-p identifier)) (string-upcase identifier))
                     (t identifier))))))
 
 (defparameter *include-paths* ())
